2ccbe9c16b69a0b84da60e22502ada8fab020432
TheWormKill <inokentiy.babushkin@googlemail.com>
2014-08-18 19:37:44 -0100

    Added new documentation to output parser
    The OutputParser class is extensivly commented, a few
    bugs / issues were found, will be adressed in a future
    update. Format specs need update.

--- output_parser.py
+++ output_parser.py
@@ -1,22 +1,33 @@
 #! /bin/env python
 
 class OutputParser:
+	# the main abstraction of the parsing process. see bottom of file for usage.
 	def __init__(self, filename):
-		self.stack = []
-		self.output = ''
-		self.lines = map(lambda x: x.strip('\r\n'), open(filename, 'rb').readlines())
-		self.prefix = ''
+		self.stack = [] # stack of current structures
+		self.output = '' # output (C-source)
+		self.lines = map(lambda x: x.strip('\r\n'), open(filename, 'rb').readlines()) # lines of ASM-source
+		self.prefix = '' # used for indentation, currently not used
 	
 	def append_to_output(self, string):
+		# append new statement to output
+		# @param string: raw value of output chunk
 		self.output += ('\n'+self.prefix).join(string.split('\n'))  
 	
 	def parse(self):
+		# processes all lines of code in order
+		# to generate output
 		for index, line in enumerate(self.lines):
-			if line.startswith(';'):
+			if line.startswith(';'): # token recognizing
 				self.parse_structure(index, line[2:])
 	
 	def push_new_element(self, string, index):
-		if string.startswith('func'):
+		# appends new structure on the stack.
+		# used for if's, else's, loops...
+		# @param string: token representing the
+		# structure to be created, see metaformat-specs(-de).txt
+		# for details.
+		# @param index: the numer of the current line in the ASM-listing
+		if string.startswith('func'): # self-explanatory...
 			print 'Function...'
 			func_name = string.split('(')[1][:-2] # get <name> from "begin func(<name>)"
 			self.stack.append(Function(func_name))
@@ -39,29 +50,35 @@ class OutputParser:
 			self.stack.append(Condition(index+1, do_param))
 	
 	def get_lines(self, first, last):
+		# used to retrieve a block of code
+		# @param first: first line to be returned
+		# @param last: first line not to be returned
+		# @ret: a list of all lines including first, but not last
+		#       and everything in between
 		return self.lines[first:last]
-			
-		#if not string in ['code', 'loop', 'if']:
-		#s	self.prefix += '\t'
 				
 	def parse_structure(self, index, line):
+		# used to parse one specific token, performing
+		# necessary operations on the stack.
+		# @param index: the index of the current line (ASM-listing)
+		# @param line: the line as a string
 		tokens = line.split(' ')
-		if tokens[0] == 'begin':
+		if tokens[0] == 'begin': # opening a new strucutre? (see specs)
 			self.push_new_element(tokens[1], index)
-			self.append_to_output(self.stack[-1].get_code_begin())
-			print 'push '+ self.stack[-1].corresponding_token
-		elif tokens[0] == 'end':
-			if tokens[1].startswith(self.stack[-1].corresponding_token):
-				print 'pop '+ self.stack[-1].corresponding_token
+			self.append_to_output(self.stack[-1].get_code_begin()) # output...
+			print 'push '+ self.stack[-1].corresponding_token # debug
+		elif tokens[0] == 'end': # popping a structure?
+			if tokens[1].startswith(self.stack[-1].corresponding_token): # data-validation
+				print 'pop '+ self.stack[-1].corresponding_token # debug
 				#if self.stack[-1].corresponding_token in ['code', 'loop', 'if']:
 				#	self.prefix = self.prefix[:-1]
 				if self.stack[-1].corresponding_token == 'code':
 					self.stack[-1].content_lines = self.get_lines(self.stack[-1].first_line, index)
 				elif self.stack[-1].corresponding_token == 'condition':
 					self.stack[-1].code_lines = self.get_lines(self.stack[-1].first_line, index)
-				self.append_to_output(self.stack[-1].get_code_end())
+				self.append_to_output(self.stack[-1].get_code_end()) # output...
 				self.stack.pop()
-		else:
+		else: # other cases, like vars and args... currently no extensive feature-set
 			if tokens[0].startswith('bytes'):
 				pass
 		
@@ -183,7 +200,9 @@ class Condition(Structure):
 			return '/**'+ self.code() +'*/){\n'
 		return '}while(/**'+ self.code() +'**/'
 
-o = OutputParser('tests/func.asm')
-o.parse()
-print '=============END OF DEBUG=============='
-print o.output
+
+if __name__ == '__main__':
+	o = OutputParser('tests/func.asm') # feed a new file...
+	o.parse() # parse it...
+	print '=============END OF DEBUG=============='
+	print o.output # print the results.
