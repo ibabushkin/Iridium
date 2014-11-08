b273e41e51b111b4f2e675bab0f365bba9086b04
TheWormKill <inokentiy.babushkin@googlemail.com>
2014-11-08 14:33:40 -0100

    Updated documentation
    [*] Text and explanations (german) - see docs/first.*
    [*] Some comments in the data-module - see defines/assembly/data.py

--- defines/assembly/data.py
+++ defines/assembly/data.py
@@ -3,20 +3,24 @@ from labels import Label
 from types import *
 
 class DataParser:
+    # intended to parse a program regarding the data it uses
+    # similar to Graph (at least consdering the level of abstraction
+    # and overall position in the system)
     def __init__(self, text):
-        self.text = text
-        self.code = self.get_code_from_text()
-        self.addressed_offsets = []
-        self.variables = []
-        self.real_variables = []
-        self.called_funcs = {}
-        self.sizes = {'qword':8, 'dword':4, 'word':2, 'byte':1}
+        self.text = text # raw code
+        self.code = self.get_code_from_text() # medium-level code representation
+        self.addressed_offsets = [] # used to determine class 0 vars
+        self.variables = [] # used for class 0 vars
+        self.real_variables = [] # class 1 vars
+        self.called_funcs = {} # for call-/ return-analysis
+        self.sizes = {'qword':8, 'dword':4, 'word':2, 'byte':1} # var sizes
         #self.hll_sizes = {8:['long long int', 'double'],
                           #4:['int', 'float'],
                           #2:['short int'],
                           #1:['char']}
         
     def get_code_from_text(self):
+        # same as in Graph. maybe we should use some Parent-Class?
         l = []
         index = 0
         for line in self.text:
@@ -32,6 +36,8 @@ class DataParser:
         return l
 
     def recognize(self):
+        # the main method to record all data availible
+        # in question
         self.get_memory_offsets_assuming_IDA()
         self.generate_variables()
         self.allocated_space = self.get_allocated_space()
@@ -61,12 +67,19 @@ class DataParser:
             print var
     
     def recognize_frontend(self):
+        # called by top-level scripts like Iridium_IDA.py
+        # to prevent errors caused by compiler-generated functions
+        # still keeping maintainability forthe code in self.recognze()
         try:
             self.recognize()
         except:
             print 'Analysis not possible!'
     
     def find_variable_by_contrast_point(self, point):
+        # a contrast point is a point in memory (on the stack)
+        # that is at the edge of some chunk of memory that is adressed
+        # directly by the code. This method makes it possible to find the
+        # corresponding variable.
         byte_offset = -self.allocated_space + point -1
         for i in self.real_variables:
             #print i.name, i.offset, self.sizes[i.size], byte_offset
@@ -74,12 +87,16 @@ class DataParser:
                 return i
     
     def get_allocated_space(self):
+        # searches for the first sub-instruction in the code, that is used
+        # to allocate space on the stack.
         for line in self.code:
             if isinstance(line, Instruction):
-                if line.mnemonic == 'sub':
+                if line.mnemonic == 'sub' and 'esp' in line.operands: # save enough, I hope
                     return int(line.operands.split(' ')[1], 16)
         
     def draw_memory_layout(self):
+        # represents directly adressed memory as chars in a chr-bar of some sort.
+        # can be drawn directly or analyzed.
         ret = ['-' for i in range(0, self.allocated_space)]
         for var in self.variables:
             for i in range(var.offset, var.offset + self.sizes[var.size]):
@@ -87,11 +104,13 @@ class DataParser:
         return ''.join(ret)
     
     def generate_variables(self):
+        # generates medium-level representations of addressed offsets
         for offset in self.addressed_offsets:
             name, size = self.get_name_and_size_of_variable_by_offset(offset)
             self.variables.append(Variable(name, size, offset))
     
     def get_name_and_size_of_variable_by_offset(self, offset):
+        # analyzes, how much space is addressed at a specific offset
         if offset in self.addressed_offsets:
             for i in self.code:
                 if isinstance(i, Instruction):
@@ -102,6 +121,7 @@ class DataParser:
                             return [name, size]
 
     def get_memory_offsets_assuming_IDA(self):
+        # parses the IDA-specific var-statements
         for i in self.code:
             if isinstance(i, Instruction):
                 if i.mnemonic.startswith('var_'):
@@ -111,6 +131,8 @@ class DataParser:
         #print self.addressed_offsets
     
     def find_pointers(self):
+        # searches for pointers using the analysis of lea-instructions
+        # will be enhanced to search for malloc()
         for index, line in enumerate(self.code):
             if line.mnemonic == 'lea':
                 destination = line.operands.split(',')[0]
@@ -119,12 +141,14 @@ class DataParser:
                     self.find_variable_by_expression(instruction.operands.split(',')[0]).pointer = True
     
     def find_variable_by_expression(self, expression):
+        # used to determine the name of a variable used in an instruction
         var_name = expression[1:-1].split('+')[-1]
         for i in self.real_variables:
             if i.name == var_name:
                 return i
 
     def find_all_function_calls(self):
+        # searches for all function calls
         ret = {}
         for index, line in enumerate(self.code):
             if isinstance(line, Instruction):
@@ -133,6 +157,7 @@ class DataParser:
         return ret
 
 class Variable:
+    # the medium-level representation of an addressed offset
     def __init__(self, name, size, offset):
         self.size = size
         self.hll_sizes = {'qword':['double', 'long long int'],
@@ -147,6 +172,7 @@ class Variable:
         self.pointer = False
     
     def __str__(self):
+        #fancy printing
         return '/'.join(self.hll_size) + ' ' + self.name + ' (' + str(self.offset) + ') Elements: ' + str(self.num_items) + ' Pointer: '+ str(self.pointer)
 
 if __name__ == '__main__':
